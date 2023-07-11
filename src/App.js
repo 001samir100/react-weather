78637ae02aade9bcfa432af524085412682f249c
Samir Shrestha <pit.sam.010@gmail.com>
2023-07-11 09:57:13 +0545

    Remove unnecessary commented codes

--- src/App.js
+++ src/App.js
@@ -14,218 +14,202 @@ import { useCallback, useEffect } from "react";
 import { useState } from "react";
 
 function App() {
-  const apiKey = "4bfb944132b93e5127a664360dbcc3c9";
-  const url = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="; //kathmandu&appid=
+	const apiKey = "4bfb944132b93e5127a664360dbcc3c9";
+	const url = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="; //kathmandu&appid=
 
-  const [input, setInput] = useState("");
-  const [data, setData] = useState({});
-  const [isLoading, setIsLoading] = useState(true);
-  const [weather, setWeather] = useState();
-  const [error, setError] = useState();
+	const [input, setInput] = useState("");
+	const [data, setData] = useState({});
+	const [isLoading, setIsLoading] = useState(true);
+	const [weather, setWeather] = useState();
+	const [error, setError] = useState();
 
-  const [isOnline, setIsOnline] = useState(false);
+	const [isOnline, setIsOnline] = useState(false);
 
-  // when search button is clicked, below function will be invoked.
-  const checkWeather = async (place) => {
-    const response = await fetch(url + place + `&appid=${apiKey}`);
-    if (response.status === 200) {
-      const json = await response.json();
-      setData(json);
-      setWeather(data.weather[0].main);
+	// when search button is clicked, below function will be invoked.
+	const checkWeather = async (place) => {
+		const response = await fetch(url + place + `&appid=${apiKey}`);
+		if (response.status === 200) {
+			const json = await response.json();
+			setData(json);
+			setWeather(data.weather[0].main);
 
-      setIsLoading(false);
+			setIsLoading(false);
 
-      console.log(data);
-    } else if (response.status >= 400) {
-      console.log("error occurred code with: " + response.status);
-    } else {
-      console.error("error occurred: " + response.text);
-      setError(error);
-      setIsLoading(true);
-    }
+			console.log(data);
+		} else if (response.status >= 400) {
+			console.log("error occurred code with: " + response.status);
+		} else {
+			console.error("error occurred: " + response.text);
+			setError(error);
+			setIsLoading(true);
+		}
+	};
 
-    // await fetch(url + place + `&appid=${apiKey}`)
-    //   .then((response) => response.json())
-    //   .then((data) => {
-    //     setData(data);
-    //     setWeather(data.weather[0].main);
+	// set image according to weather
+	const weatherImage = (weatherImgName = "Clear") => {
+		// console.log(`weather image : ${weatherImgName}`);
+		switch (weatherImgName) {
+			case "Clouds": {
+				// return cloudImg;
+				return <img src={cloudImg} alt="Clouds" />;
+			}
+			case "Winds": {
+				// return windImg;
+				return <img src={windImg} alt="Winds" />;
+			}
+			case "Clear": {
+				// return weatherImg;
+				return <img src={clearImg} alt="Clear" />;
+			}
+			case "Rain": {
+				// return rainImg;
+				return <img src={rainImg} alt="Rain" />;
+			}
+			case "Drizzle": {
+				// return drizzleImg;
+				return <img src={drizzleImg} alt="Drizzle" />;
+			}
+			case "Mist": {
+				// return mistImg;
+				return <img src={mistImg} alt="Mist" />;
+			}
+			case "Snow": {
+				// return snowImg;
+				return <img src={snowImg} alt="Snow" />;
+			}
+			case "Haze": {
+				return <img src={hazeImg} alt="Snow" />;
+			}
+			default:
+				return <img src={clearImg} alt="Clear" />;
+		}
+	};
 
-    //     setIsLoading(false);
+	const handleInputChange = useCallback((e) => {
+		setInput(e.target.value);
+	}, []);
 
-    //     console.log(data);
-    //   })
-    //   .catch((error) => {
-    //     console.error(error);
-    //     setError(error);
-    //     setIsLoading(true);
-    //   });
-  };
+	const checkInternetConnection = async () => {
+		try {
+			const response = await fetch("https://www.google.com");
+			return response.status === 200;
+		} catch (error) {
+			return false;
+		}
+	};
 
-  // set image according to weather
-  const weatherImage = (weatherImgName = "Clear") => {
-    console.log(`weather image : ${weatherImgName}`);
-    switch (weatherImgName) {
-      case "Clouds": {
-        // return cloudImg;
-        return <img src={cloudImg} alt="Clouds" />;
-      }
-      case "Winds": {
-        // return windImg;
-        return <img src={windImg} alt="Winds" />;
-      }
-      case "Clear": {
-        // return weatherImg;
-        return <img src={clearImg} alt="Clear" />;
-      }
-      case "Rain": {
-        // return rainImg;
-        return <img src={rainImg} alt="Rain" />;
-      }
-      case "Drizzle": {
-        // return drizzleImg;
-        return <img src={drizzleImg} alt="Drizzle" />;
-      }
-      case "Mist": {
-        // return mistImg;
-        return <img src={mistImg} alt="Mist" />;
-      }
-      case "Snow": {
-        // return snowImg;
-        return <img src={snowImg} alt="Snow" />;
-      }
-      case "Haze": {
-        return <img src={hazeImg} alt="Snow" />;
-      }
-      default:
-        return <img src={clearImg} alt="Clear" />;
-    }
-  };
+	useEffect(() => {
+		const handleOnlineStatus = () => setIsOnline(true);
+		const handleOfflineStatus = () => setIsOnline(false);
 
-  const handleInputChange = useCallback((e) => {
-    setInput(e.target.value);
-  }, []);
+		window.addEventListener("online", handleOnlineStatus);
+		window.addEventListener("offline", handleOfflineStatus);
 
-  const checkInternetConnection = async () => {
-    try {
-      const response = await fetch("https://www.google.com");
-      return response.status === 200;
-    } catch (error) {
-      return false;
-    }
-  };
+		return () => {
+			window.removeEventListener("online", handleOnlineStatus);
+			window.removeEventListener("offline", handleOfflineStatus);
+		};
+	}, []);
 
-  useEffect(() => {
-    const handleOnlineStatus = () => setIsOnline(true);
-    const handleOfflineStatus = () => setIsOnline(false);
+	useEffect(() => {
+		if (!isOnline) {
+			const intervalId = setInterval(async () => {
+				const isConnected = await checkInternetConnection();
+				setIsOnline(isConnected);
+			}, 5000);
 
-    window.addEventListener("online", handleOnlineStatus);
-    window.addEventListener("offline", handleOfflineStatus);
+			return () => clearInterval(intervalId);
+		} else {
+			setIsOnline(true);
+		}
+	}, [isOnline]);
 
-    return () => {
-      window.removeEventListener("online", handleOnlineStatus);
-      window.removeEventListener("offline", handleOfflineStatus);
-    };
-  }, []);
+	if (error) {
+		return (
+			<div>
+				<h2>Error occurred : ${error}</h2>
+			</div>
+		);
+	} else {
+		return (
+			<>
+				<div className="App">
+					<div className="card">
+						{/* for search bar */}
+						<div className="search">
+							<input
+								placeholder="Search place"
+								type="text"
+								spellCheck="false"
+								value={input}
+								onChange={handleInputChange}
+							/>
+							<button
+								onClick={() => {
+									checkWeather(input);
+								}}
+							>
+								<img src={srcImg} alt="Search button" />
+							</button>
+						</div>
 
-  useEffect(() => {
-    if (!isOnline) {
-      const intervalId = setInterval(async () => {
-        const isConnected = await checkInternetConnection();
-        setIsOnline(isConnected);
-      }, 5000);
+						{/* online status indicator */}
+						<div className="flex">
+							{isOnline ? (
+								<p className="statusText">Online</p>
+							) : (
+								<p className="statusText">Offline</p>
+							)}
+						</div>
 
-      return () => clearInterval(intervalId);
-    } else {
-      setIsOnline(true);
-    }
-  }, [isOnline]);
+						{/* for weather details */}
+						{isLoading ? (
+							<></>
+						) : (
+							<>
+								<div className="details">
+									<div className="col">
+										<h2>Min</h2>
+										<h3>{data.main.temp_min}&deg;C</h3>
+									</div>
+									<div className="col">
+										{weatherImage(weather)}
+										<h1>{data.main.temp} &deg;C</h1>
+										<h2>{data.name}</h2>
+									</div>
+									<div className="col">
+										<h2>Max</h2>
+										<h3>{data.main.temp_max}&deg;C</h3>
+									</div>
+								</div>
+								<div className="detail">
+									<div className="row">
+										<div className="flex">
+											<img src={humidityImg} alt="" style={{ width: "80px" }} />
+											<div className="col">
+												<h2>{data.main.humidity}%</h2>
+												<h3>Humidity</h3>
+											</div>
+										</div>
+									</div>
 
-  if (error) {
-    return (
-      <div>
-        <h2>Error occurred : ${error}</h2>
-      </div>
-    );
-  } else {
-    return (
-      <>
-        <div className="App">
-          <div className="card">
-            {/* for search bar */}
-            <div className="search">
-              <input
-                placeholder="Search place"
-                type="text"
-                spellCheck="false"
-                value={input}
-                onChange={handleInputChange}
-              />
-              <button
-                onClick={() => {
-                  checkWeather(input);
-                }}
-              >
-                <img src={srcImg} alt="Search button" />
-              </button>
-            </div>
-
-            {/* online status indicator */}
-            <div className="flex">
-              {isOnline ? (
-                <p className="statusText">Online</p>
-              ) : (
-                <p className="statusText">Offline</p>
-              )}
-            </div>
-
-            {/* for weather details */}
-            {isLoading ? (
-              <></>
-            ) : (
-              <>
-                <div className="details">
-                  <div className="col">
-                    <h2>Min</h2>
-                    <h3>{data.main.temp_min}&deg;C</h3>
-                  </div>
-                  <div className="col">
-                    {weatherImage(weather)}
-                    <h1>{data.main.temp} &deg;C</h1>
-                    <h2>{data.name}</h2>
-                  </div>
-                  <div className="col">
-                    <h2>Max</h2>
-                    <h3>{data.main.temp_max}&deg;C</h3>
-                  </div>
-                </div>
-                <div className="detail">
-                  <div className="row">
-                    <div className="flex">
-                      <img src={humidityImg} alt="" style={{ width: "80px" }} />
-                      <div className="col">
-                        <h2>{data.main.humidity}%</h2>
-                        <h3>Humidity</h3>
-                      </div>
-                    </div>
-                  </div>
-
-                  <div className="row">
-                    <div className="flex">
-                      <img src={windImg} alt="" style={{ width: "80px" }} />
-                      <div className="col">
-                        <h2>{data.wind.speed} km/h</h2>
-                        <h3>wind</h3>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </>
-            )}
-          </div>
-        </div>
-      </>
-    );
-  }
+									<div className="row">
+										<div className="flex">
+											<img src={windImg} alt="" style={{ width: "80px" }} />
+											<div className="col">
+												<h2>{data.wind.speed} km/h</h2>
+												<h3>wind</h3>
+											</div>
+										</div>
+									</div>
+								</div>
+							</>
+						)}
+					</div>
+				</div>
+			</>
+		);
+	}
 }
 
 export default App;
